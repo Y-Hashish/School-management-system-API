a6033ef566d903a92463ac02166b6155b33d3a3b
Hashish <Hashish@DESKTOP-E43BSTS>
2025-02-09 00:02:27 +0200

    login and register end points (Auth controller)

---
+++ School management system/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School_management_system.Models;
+using School_management_system.Services;
+
+namespace School_management_system.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> register(RegisterModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _authService.RegisterAsync(model);
+            if(!result.IsAuth)
+            {
+                BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> login(LoginModel model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _authService.GenerateToken(model);
+            if(!result.IsAuth)
+                BadRequest(result.Message);
+            return Ok(result);
+        }
+    }
+}
